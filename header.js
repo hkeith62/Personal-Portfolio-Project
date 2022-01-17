2253e6c9e942ece84186681fb748e6b97380847e
Keith Hall <hkeith62@gmail.com>
2022-01-16 21:21:06 -0600

    Portfolio Project

--- header.js
+++ header.js
@@ -9,14 +9,14 @@ class Header extends HTMLElement
           <div id="header">
             <span id="logo" style="background-color:#094514; width:60px; height:41px; color:#69F483">KH</span>
  		      <div class="the-hamburger"><a href="javascript:void(0);" class="icon" onclick="myFunction()"><i class="fa fa-bars fa-lg"></i></a></div>
-                <ul class="nav-container">
+                <ul class="nav-container ">
                   <li><a href="index.html">Home</a></li>
                   <li><a href="projects.html">Projects</a></li>
-                  <li><a href="./web-335/hall-diagrams.html">Database Diagrams</a></li>
+                  <li><a href="database-diagrams.html">Database Diagrams</a></li>
                   <li><a href="unit-tests.html">API Unit Tests</a></li>
-				  <li><a href="./web-430/hall-tech-value-stream.html">Devops</a></li>
-                  <li><a href="about.html">Resume</a></li>
-                  <li><a href="resume.html">About</a></li>
+				  <li><a href="devops.html">Devops</a></li>
+                  <li><a href="resume.html">Resume</a></li>
+                  <li><a href="about.html">About</a></li>
                </ul>
           </div>`
         ;
