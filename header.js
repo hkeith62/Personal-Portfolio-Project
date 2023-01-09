17fbaa2940dd15c9aeceedd5affca1ee0b031503
Keith Hall <hkeith62@gmail.com>
2023-01-09 15:42:28 -0600

    additions

--- header.js
+++ header.js
@@ -8,7 +8,6 @@ class Header extends HTMLElement
         this.innerHTML = `
           <div id="header">
             <span id="logo" style="background-color:#094514; width:60px; height:41px; margin-left:60px; color:#69F483">KH</span>
- 		      <div class="the-hamburger"><a href="javascript:void(0);" class="icon" onclick="myFunction()"><i class="fa fa-bars fa-lg"></i></a></div>
                 <ul class="nav-container ">
                   <li><a href="index.html">Home</a></li>
                   <li><a href="projects.html">Projects</a></li>
